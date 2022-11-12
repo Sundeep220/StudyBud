819a190e2c428f2acc06177a717d65652dbdfd91
Sundeep220 <sundeepkumarsingh0@gmail.com>
2022-11-13 02:33:26 +0530

    Added models for Room, topic & messages along with views and urls

--- base/views.py
+++ base/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import *
 # Create your views here.
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html',context)
+
+def room(request, id):
+    room = Room.objects.get(id=id)
+    messages = room.message_set.all()
+    context = {'room': room, 'messages': messages}
+    return render(request, 'base/room.html', context)
--- studybud/settings.py
+++ studybud/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base.apps.BaseConfig'
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'studybud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # configuration for templates folder outside app
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
