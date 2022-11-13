93bbab26db970afc91db255b6264485e62b275f4
Sundeep220 <sundeepkumarsingh0@gmail.com>
2022-11-13 15:07:29 +0530

    Added authentication facilities as well as modified room page to dispaly some content

--- base/models.py
+++ base/models.py
@@ -6,17 +6,20 @@ from django.contrib.auth.models import User
 class Topic(models.Model):
     name = models.CharField(max_length=200)
 
-    def __str__(Self):
-        return Self.name
+    def __str__(self):
+        return self.name
 
 class Room(models.Model):
     host=  models.ForeignKey(User, on_delete=models.SET_NULL, null = True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null = True)
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
-    # participants = 
+    participants = models.ManyToManyField(User, related_name='participants',blank=True) 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.name
